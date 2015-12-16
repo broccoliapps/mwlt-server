02b34a942f42436ed4c312d915073c8d6175494e
Danny Delott <dannydelott@gmail.com>
2015-12-15 22:47:20 -0800

    Add sequelize with promises

--- server.js
+++ server.js
@@ -1,12 +1,68 @@
-var koa = require('koa');
+// Import npm packages
+var koa       = require('koa'),
+    Sequelize = require('sequelize');
+
+// Set up environment variables
+require('dotenv').load();
+const PORT       = process.env.PORT || 3000,
+      DB_NAME    = process.env.DB_NAME,
+      DB_USER    = process.env.DB_USER,
+      DB_PASS    = process.env.DB_PASS,
+      DB_PORT    = process.env.DB_PORT,
+      DB_DIALECT = "postgres";
+
+// Initialize koa
 var app = koa();
 
-const PORT = 3000;
+// Instantiate sequelize
+var sequelize = null;
+startSequelize()
+  .then(setRoutes)
+  .catch(logSequelizeError)
+  .done(startApp);
 
-app.use(function *(){
-  this.body = 'Hello World';
-});
+/**
+ * startSequelize
+ * Uses env variables to begin the authentication process with postgres.
+ * @return Promise
+ */
+function startSequelize(err) {
+  var sequelizeOptions = {
+    dialect: DB_DIALECT,
+    port:    DB_PORT
+  };
 
-app.listen(3000, () => {
-  console.log(`Listening on port: ${PORT}`);
-});
+  sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, sequelizeOptions);
+
+  return sequelize.authenticate();
+}
+
+/**
+ * setRoutes
+ * Takes the global app and adds routes and middleware.
+ */
+function setRoutes(err) {
+  app.use(function *(){
+    this.body = 'Hello World';
+  });
+  console.log('Database connection has been established successfully.');
+}
+
+/**
+ * logSequelizeError
+ * Fires if sequelize instance can't connect to database.
+ */
+function logSequelizeError(err){
+  console.log('Unable to connect to the database:', err);
+}
+
+/**
+ * startAp
+ * Begin listening on the specified port.
+ */
+function startApp(){
+  app.listen(PORT, () => {
+    console.log(`Listening on localhost:${PORT}`);
+  });
+}
+
